0771d24da666ae1c631518ba7ca210377ff77049
tanakmura <tanakmura@gmail.com>
2020-07-09 23:43:58 +0900

    flash

---
+++ flash/README.md
@@ -0,0 +1,1 @@
+copy jtag bitfile(https://github.com/quartiq/bscan_spi_bitstreams/blob/master/bscan_spi_xc7a35t.bit) into this directory.
---
+++ flash/flash.tcl
@@ -0,0 +1,22 @@
+interface ftdi
+ftdi_vid_pid 0x0403 0x6010
+ftdi_channel 0
+ftdi_layout_init 0x0098 0x008b
+reset_config none
+
+source [find cpld/xilinx-xc7.cfg]
+source [find cpld/jtagspi.cfg]
+
+adapter_khz 10000
+
+init
+jtagspi_init 0 bscan_spi_xc7a35t.bit
+set wd [pwd]
+
+flash erase_address 0x000000 0x400000
+flash write_image $wd/../jisaku_pc/jisaku_pc.runs/impl_1/jisaku_pc_top.bit 0x000000
+
+flash erase_address 0x800000 0x010000
+flash write_image ../demo/blink_led 0x800000
+exit
+
